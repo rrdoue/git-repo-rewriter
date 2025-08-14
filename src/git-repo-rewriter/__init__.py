793e098caf5d2a8fa371853d1ebf7313ab1c80fb
Ray Doue <rrdoue@gmail.com>
2025-08-14 17:31:14 -0500

    We were in the process of running a git pull origin --rebase devel and git needs to execute some kind of merge, so we are trying to understand and continue the process.

--- src/git-repo-rewriter/__init__.py
+++ src/git-repo-rewriter/__init__.py
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 # Blank or null __init__.py file
+=======
+def main() -> None:
+    print("Hello from git-repo-rewriter!")
+>>>>>>> abcb1fe (Adding this file using the main branch since we had added the other src file using main, and we are in the process of setting up the project anyway.)
